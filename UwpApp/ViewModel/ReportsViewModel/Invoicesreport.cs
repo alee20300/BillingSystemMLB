fe143e1fddf7815f24b610fa4abe3cc21ada1723
alee20300 <46748867+alee20300@users.noreply.github.com>
2020-12-02 15:41:36 +0500

    adding  invoice view

---
+++ UwpApp/ViewModel/ReportsViewModel/Invoicesreport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UwpApp.ViewModel.ReportsViewModel
+{
+   public class Invoicesreport :BindableBase
+
+    {
+        public Invoicesreport()
+        {
+            LoadInvoice();
+        }
+
+        public void LoadInvoice()
+        {
+            
+        
+        
+        }
+    }
+ }
