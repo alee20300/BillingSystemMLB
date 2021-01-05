96254595b3be63e78947f130403dabc4b5fbd14d
Timoty Cross <txcross@zonemail.clpccd.edu>
2021-01-05 10:30:29 +0500

    implimneting multipayment (backend)

---
+++ Domin/Data/PaymentDetail.cs
@@ -0,0 +1,19 @@
+﻿using Domin.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domin.Data
+{
+    
+
+  public  class PaymentDetail
+    {
+        public int PaymentId { get; set; }
+        public Memo Memo { get; set; }
+        public Account Account { get; set; }
+
+        public Decimal Amount { get; set; }
+        
+    }
+}
